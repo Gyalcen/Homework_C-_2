6d3d7168950f4fdee633e4846edba50370c77370
Kuular_G <neverhoodst@mail.ru>
2022-09-26 10:03:02 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+Задача 1.
+
+Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32 (Console.ReadLine());
+{
+int FirstNumber = (num / 10) % 10;
+
+Console.WriteLine(FirstNumber);
+};
+*/
+
+/*
+
+Задача 2.
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(num);
+if (anyNumberText.Length > 2)
+{
+  Console.WriteLine("третья цифра " + anyNumberText[2]);
+}
+else 
+{
+  Console.WriteLine("третьей цифры нет");
+};
+*/
+
+/* 
+
+Задача 3.
+
+Console.Write("Введи цифру, которая обозначает день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void Day (int dayNumber) 
+{
+  if (dayNumber == 6 || dayNumber == 7) 
+  {
+  Console.WriteLine("Выходной");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) 
+  {
+    Console.WriteLine("В неделе семь дней");
+  }
+  else Console.WriteLine("Будний день");
+}
+
+Day(dayNumber);
+*/
